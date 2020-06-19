5005147e3205669f6aa58bda9a0c2e265ece1530
Azhagiri Muthusamy <giri80.m@gmail.com>
2020-06-19 17:02:22 +0530

    First IFET react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import ece2002_pic from './img/ece2002_batch.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>IFET ALUMINI ECE(1998-2002)</h1>
+      <h2>Author : IT Developers</h2>
+      <img src={ece2002_pic} alt="profile-image" />
+    </div>
+  );
+}
+
+export default App;
