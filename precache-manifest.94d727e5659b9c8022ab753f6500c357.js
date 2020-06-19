153f438630da2e83685fae4d1edbd3d78d89d0a3
Azhagiri Muthusamy <giri80.m@gmail.com>
2020-06-19 17:21:37 +0530

    Updates

---
+++ precache-manifest.94d727e5659b9c8022ab753f6500c357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45be641003a22c50d51cf49cf9e4573",
+    "url": "/ifetalumini/index.html"
+  },
+  {
+    "revision": "e04cb164019bd42b0d1d",
+    "url": "/ifetalumini/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "a0a6860e61034198d778",
+    "url": "/ifetalumini/static/js/2.22604fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ifetalumini/static/js/2.22604fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04cb164019bd42b0d1d",
+    "url": "/ifetalumini/static/js/main.7f8b1453.chunk.js"
+  },
+  {
+    "revision": "7f5b49a5c7cc247b5687",
+    "url": "/ifetalumini/static/js/runtime-main.9852f773.js"
+  },
+  {
+    "revision": "0892de5f27c520cd32090fe73688b068",
+    "url": "/ifetalumini/static/media/ece2002_batch.0892de5f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ifetalumini/static/media/logo.5d5d9eef.svg"
+  }
+]);
